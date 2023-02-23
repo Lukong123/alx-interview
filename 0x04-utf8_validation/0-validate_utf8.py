39e9c986e24382788d3d7e6850a113cebc902aa1
anne <annemachinda@gmail.com>
2023-02-23 14:24:05 +0100

    utf8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,49 +3,37 @@
 
      
 def validUTF8(data):
+    """Determines if a given data set
+    represents a valid utf-8 encoding
     """
-    :type data: List[int]
-    :rtype: bool
-    """
+    number_bytes = 0
 
-    # Number of bytes in the current UTF-8 character
-    n_bytes = 0
+    mask_1 = 1 << 7
+    mask_2 = 1 << 6
 
-    # For each integer in the data array.
-    for num in data:
+    for i in data:
 
-        # Get the binary representation. We only need
-        # the least significant 8 bits for any given number.
-        bin_rep = format(num, '#010b')[-8:]
+        mask_byte = 1 << 7
 
-        # If this is the case then we are to start
-        # processing a new UTF-8 character.
-        if n_bytes == 0:
+        if number_bytes == 0:
 
-            # Get the number of 1s in the beginning of the string.
-            for bit in bin_rep:
-                if bit == '0':
-                    break
-                n_bytes += 1
+            while mask_byte & i:
+                number_bytes += 1
+                mask_byte = mask_byte >> 1
 
-            # 1 byte characters
-            if n_bytes == 0:
+            if number_bytes == 0:
                 continue
 
-            # Invalid scenarios according to the rules of the problem.
-            if n_bytes == 1 or n_bytes > 4:
+            if number_bytes == 1 or number_bytes > 4:
                 return False
+
         else:
-            # Else, we are processing integers which
-            # represent bytes which are a part of
-            # a UTF-8 character. So, they must adhere
-            # to the pattern `10xxxxxx`.
-            if not (bin_rep[0] == '1' and bin_rep[1] == '0'):
+            if not (i & mask_1 and not (i & mask_2)):
                 return False
 
-        # We reduce the number of bytes to process by 1 after each integer.
-        n_bytes -= 1
+        number_bytes -= 1
 
-    # This is for the case where we might not have the complete data for
-    # a particular UTF-8 character.
-    return n_bytes == 0
+    if number_bytes == 0:
+        return True
+
+    return False
