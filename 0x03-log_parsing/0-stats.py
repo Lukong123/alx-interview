4ef3f6ab65cef4a39555bacc6d8434769de1ec5d
anne <annemachinda@gmail.com>
2023-02-23 14:10:01 +0100

    stats

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
-"""a script that reads stdin line by line and computes metrics"""
+""" a script that reads stdin line by line and computes metrics: """
+
 import sys
 
-j = 0
-total_size = 0
-status = [200, 301, 400, 401, 403, 404, 405, 500]
-dict = {200:0, 301:0, 400:0, 401:0, 403:0, 404:0, 405:0, 500:0}
 
-def printstatus(dict, total_size):
+i = 0
+totalsize = 0
+status = [200, 301, 400, 401, 403, 404, 405, 500]
+dict = {200: 0, 301: 0, 400: 0, 401: 0,
+        403: 0, 404: 0, 405: 0, 500: 0}
+
+
+def printstatus(dict, totalsize):
     """print status"""
-    print("File size: {}".format(total_size))
+    print("File size: {}".format(totalsize))
     for item in status:
         if dict[item]:
             print("{}: {}".format(item, dict[item]))
@@ -18,20 +22,20 @@ def printstatus(dict, total_size):
 if __name__ == "__main__":
     try:
         for line in sys.stdin:
-            if j != 0 and j % 10 == 0:
-                printstatus(dict, total_size)
+            if i != 0 and i % 10 == 0:
+                printstatus(dict, totalsize)
             mylist = line.split(' ')
             if len(mylist) != 9:
                 continue
-            j += 1
+            i += 1
             try:
-                total_size += int(mylist[-1])
+                totalsize += int(mylist[-1])
                 stat = int(mylist[-2])
             except Exception:
                 pass
             if stat in status:
                 dict[stat] += 1
-        printstatus(dict, total_size)
+        printstatus(dict, totalsize)
     except KeyboardInterrupt:
-        printstatus(dict, total_size)
+        printstatus(dict, totalsize)
         raise
