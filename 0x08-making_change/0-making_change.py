cdcddcefc42d6bafeecfde3b86e8e5cc4ece4a1b
anne <annemachinda@gmail.com>
2023-02-23 13:27:12 +0100

    making change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" determining the fewest number of coins to be given as change"""
+
+def makeChange(coins, total):
+    '''Returns fewest number of coins needed'''
+    if total < 1:
+        return 0
+    coins.sort()
+    coins.reverse()
+    change = 0
+    for coin in coins:
+        if total <= 0:
+            break
+        tmp = total // coin
+        change += tmp
+        total -= (tmp * coin)
+    if total != 0:
+        return -1
+    return change
